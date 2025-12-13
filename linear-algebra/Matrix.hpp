a2d835d4691ad8853e8664072d6769523a4e390f
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2025-12-13 22:20:01 +0800

    Implement Matrix structure with multiplication and power

---
+++ linear-algebra/Matrix.hpp
@@ -0,0 +1,27 @@
+struct Matrix {
+  vector<vector<int>> a;
+  int n;
+  Matrix(int n_) {
+    n = n_;
+    a.resize(n);
+    for (int i=0; i<n; i++) a[i].resize(n);
+  }
+  Matrix operator*(Matrix other) {
+    Matrix product(n);
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        product.a[i][j] = 1e18;
+        for (int k=0; k<n; k++) {
+          product.a[i][j] = min(product.a[i][j], a[i][k] + other.a[k][j]);
+        }
+      }
+    }
+    return product;
+  }
+};
+Matrix pow(Matrix b, int p) {
+  if (p == 1) return b;
+  Matrix r = pow(b, p >> 1);
+  if (p & 1) return r * r * b;
+  return r * r;
+}
