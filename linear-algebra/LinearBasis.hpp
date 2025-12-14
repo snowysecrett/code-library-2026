222efa5cfbd688010a34d86457e71525309ffc35
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2025-12-15 05:17:05 +0800

    Add LinearBasis struct for linear algebra operations

---
+++ linear-algebra/LinearBasis.hpp
@@ -0,0 +1,43 @@
+struct LinearBasis {
+  int B = 0, sz = 1;
+  vi has, basis;
+  LinearBasis() {}
+  LinearBasis(int B_): B(B_), has(vi(B_)), basis(vi(B_)) {}
+  void insert(int x) {
+    for (int i=B-1; i>=0; i--) {
+      if (!(x & (1 << i))) continue;
+      if (has[i]) x ^= basis[i];
+      else {
+        has[i] = 1, basis[i] = x, sz <<= 1;
+        return;
+      }
+    }
+  }
+  bool count(int x) {
+    for (int i=B-1; i>=0; i--) {
+      if (!(x & (1 << i))) continue;
+      if (has[i]) x ^= basis[i];
+      else return 0;
+    }
+    return 1;
+  }
+  int max() {
+    int ret = 0;
+    for (int i=B-1; i>=0; i--) {
+      if (!(ret & (1 << i)) && has[i]) {
+        ret ^= basis[i];
+      }
+    }
+    return ret;
+  }
+  void merge(LinearBasis other) {
+    assert(B == other.B);
+    for (int i=B-1; i>=0; i--) {
+      if (other.has[i]) insert(other.basis[i]);
+    }
+  }
+};
+LinearBasis merge(LinearBasis x, LinearBasis y) {
+  x.merge(y);
+  return x;
+}
