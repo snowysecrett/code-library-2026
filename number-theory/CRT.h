3c47ef1c3826110c6ca63f867c79cb8b4ea779cd
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2025-12-31 10:03:26 +0800

    Clean up comments in CRT.h
    Removed author and description comments from CRT.h

--- number-theory/CRT.h
+++ number-theory/CRT.h
@@ -1,11 +1,3 @@
-/**
- * Author: Unknown
- * Date: 2002-09-15
- * Source: predates tinyKACTL
- * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
- * you just need gcd, use the built in \texttt{\_\_gcd} instead.
- * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
- */
 #pragma once
 
 ll euclid(ll a, ll b, ll &x, ll &y) {
