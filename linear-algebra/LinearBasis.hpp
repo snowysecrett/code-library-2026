8848f91f1aac74a3ea964511869c4f4fb67041b7
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-01-15 07:10:45 +0800

    Refactor insert method and add rank and kth methods

--- linear-algebra/LinearBasis.hpp
+++ linear-algebra/LinearBasis.hpp
@@ -3,15 +3,16 @@ struct LinearBasis {
   vi has, basis;
   LinearBasis() {}
   LinearBasis(int B_): B(B_), has(vi(B_)), basis(vi(B_)) {}
-  void insert(int x) {
+  bool insert(int x) {
     while (x) {
       int i = 31 - __builtin_clz(x);
       if (has[i]) x ^= basis[i];
       else {
         has[i] = 1, basis[i] = x;
-        return;
+        return 1;
       }
     }
+    return 0;
   }
   bool count(int x) {
     while (x) {
@@ -35,6 +36,34 @@ struct LinearBasis {
     for (int i=0; i<B; i++) {
       if (other.has[i]) insert(other.basis[i]);
     }
+  }
+  int rank(int x) { 
+    if (!count(x)) return -1;
+    int ans = 0, sz = 1;
+    for (int i=0; i<B; i++) if (has[i]) sz <<= 1;
+    for (int i=B-1; i>=0; i--) {
+      if (has[i]) {
+        sz >>= 1;
+        if (x & (1ll << i)) ans += sz;
+      }
+    }
+    return ans + 1;
+  }
+  int kth(int k) {
+    int sz = 1;
+    for (int i=0; i<B; i++) if (has[i]) sz <<= 1;
+    if (k < 1 || k > sz) return -1;
+    int ans = 0;
+    sz >>= 1;
+    k--;
+    for (int i=B-1; i>=0; i--) {
+      if (has[i]) {
+        if ((k & sz) && (ans & (1ll << i)) == 0) ans ^= basis[i];
+        else if (!(k & sz) && (ans & (1ll << i))) ans ^= basis[i];
+        sz >>= 1;
+      }
+    }
+    return ans;
   }
 };
 LinearBasis merge(LinearBasis x, LinearBasis y) {
