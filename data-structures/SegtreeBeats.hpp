cb0429413a56ee69df9c422e5193936fd3c9f36a
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-02-03 20:46:05 +0000

    Refactor SegtreeBeats structure and methods

--- data-structures/SegtreeBeats.hpp
+++ data-structures/SegtreeBeats.hpp
@@ -4,181 +4,103 @@
  * Status: tested on LibChecker.
  */
 struct SegtreeBeats {
-  int stok;
-  const ll extr = 2e18;
-  struct node {
-    ll max1, max2, maxc;
-    ll min1, min2, minc;
-    ll lazy, sum;
+  enum Type { CHMIN, CHMAX, ADD };
+  int n;
+  static const ll INF = 2e18;
+  struct Node {
+    ll mx1, mx2, mxc, mn1, mn2, mnc, sum, lazy;
     int l, r;
-  };
-  vector<node> a;
-  void pushtag_max(int idx, ll val) {
-    if (val >= a[idx].max1) return;
-    a[idx].sum -= (a[idx].max1 - val) * a[idx].maxc;
-    a[idx].max1 = val;
-    if (a[idx].l == a[idx].r) {
-      a[idx].min1 = val;
-    }
-    else {
-      if (a[idx].min1 >= val) {
-        a[idx].min1 = val;
-        a[idx].min2 = extr;
-        a[idx].minc = a[idx].r - a[idx].l + 1;
-      }
-      else if (a[idx].min2 > val && a[idx].min2 != extr) {
-        a[idx].min2 = val;
-      }
-    }
+  }; 
+  vector<Node> st;
+  void apply_chmin(Node &ts, int v) {
+    if (ts.mx1 <= v) return;
+    ts.sum += (v - ts.mx1) * ts.mxc;
+    if (ts.mn1 == ts.mx1) ts.mn1 = v;
+    else if (ts.mn2 == ts.mx1) ts.mn2 = v;
+    ts.mx1 = v;
   }
-  void pushtag_min(int idx, ll val) {
-    if (val <= a[idx].min1) return;
-    a[idx].sum += (val - a[idx].min1) * a[idx].minc;
-    a[idx].min1 = val;
-    if (a[idx].l == a[idx].r) {
-      a[idx].max1 = val;
-    }
-    else {
-      if (a[idx].max1 <= val) {
-        a[idx].max1 = val;
-        a[idx].max2 = -extr;
-        a[idx].maxc = a[idx].r - a[idx].l + 1;
-      }
-      else if (a[idx].max2 < val && a[idx].max2 != -extr) {
-        a[idx].max2 = val;
-      }
-    }
+  void apply_chmax(Node &ts, int v) {
+    if (ts.mn1 >= v) return;
+    ts.sum += (v - ts.mn1) * ts.mnc;
+    if (ts.mx1 == ts.mn1) ts.mx1 = v;
+    else if (ts.mx2 == ts.mn1) ts.mx2 = v;
+    ts.mn1 = v;
   }
-  void pushtag_add(int idx, ll val) {
-    a[idx].max1 += val;
-    if (a[idx].max2 != -extr) a[idx].max2 += val;
-    a[idx].min1 += val;
-    if (a[idx].min2 != extr) a[idx].min2 += val;
-    a[idx].lazy += val;
-    a[idx].sum += val * (a[idx].r - a[idx].l + 1);
+  void apply_add(Node &ts, int v) {
+    ts.mx1 += v; 
+    if (ts.mx2 != -INF) ts.mx2 += v;
+    ts.mn1 += v; 
+    if (ts.mn2 != INF) ts.mn2 += v;
+    ts.sum += v * (ts.r - ts.l + 1), ts.lazy += v;
   }
-  void pushdown(int idx) {
-    if (a[idx].lazy) {
-      pushtag_add((idx<<1)+1, a[idx].lazy);
-      pushtag_add((idx<<1)+2, a[idx].lazy);
-      a[idx].lazy = 0;
+  void push_down(int idx) {
+    int ch = (idx<<1);
+    while (++ch <= (idx<<1) + 2) {
+      if (st[idx].lazy) apply_add(st[ch], st[idx].lazy);
+      apply_chmin(st[ch], st[idx].mx1);
+      apply_chmax(st[ch], st[idx].mn1);
     }
-    pushtag_max((idx<<1)+1, a[idx].max1);
-    pushtag_max((idx<<1)+2, a[idx].max1);
-    pushtag_min((idx<<1)+1, a[idx].min1);
-    pushtag_min((idx<<1)+2, a[idx].min1);
+    st[idx].lazy = 0;
   }
-  void pushup(int idx) {
-    ll max1, max2, maxc;
-    ll min1, min2, minc;
-    ll lazy, sum;
-    ll l, r;
-    a[idx].max1 = max(a[(idx<<1)+1].max1, a[(idx<<1)+2].max1);
-    a[idx].max2 = (a[(idx<<1)+1].max1 == a[(idx<<1)+2].max1 ?
-      max(a[(idx<<1)+1].max2, a[(idx<<1)+2].max2) :
-      (a[(idx<<1)+1].max1 < a[(idx<<1)+2].max1 ?
-      max(a[(idx<<1)+1].max1, a[(idx<<1)+2].max2) : 
-      max(a[(idx<<1)+1].max2, a[(idx<<1)+2].max1)
-    ));
-    a[idx].maxc = (a[(idx<<1)+1].max1 == a[(idx<<1)+2].max1 ? 
-      a[(idx<<1)+1].maxc + a[(idx<<1)+2].maxc :
-      (a[(idx<<1)+1].max1 < a[(idx<<1)+2].max1 ? 
-      a[(idx<<1)+2].maxc : a[(idx<<1)+1].maxc)
-    );
-    a[idx].min1 = min(a[(idx<<1)+1].min1, a[(idx<<1)+2].min1);
-    a[idx].min2 = (a[(idx<<1)+1].min1 == a[(idx<<1)+2].min1 ?
-      min(a[(idx<<1)+1].min2, a[(idx<<1)+2].min2) :
-      (a[(idx<<1)+1].min1 > a[(idx<<1)+2].min1 ?
-      min(a[(idx<<1)+1].min1, a[(idx<<1)+2].min2) : 
-      min(a[(idx<<1)+1].min2, a[(idx<<1)+2].min1)
-    ));
-    a[idx].minc = (a[(idx<<1)+1].min1 == a[(idx<<1)+2].min1 ? 
-      a[(idx<<1)+1].minc + a[(idx<<1)+2].minc :
-      (a[(idx<<1)+1].min1 > a[(idx<<1)+2].min1 ? 
-      a[(idx<<1)+2].minc :
-      a[(idx<<1)+1].minc)
-    );
-    a[idx].sum = a[(idx<<1)+1].sum + a[(idx<<1)+2].sum;
+  Node push_up(const Node& a, const Node& b) {
+    Node res;
+    res.mx1 = max(a.mx1, b.mx1);
+    if (a.mx1 == b.mx1) res.mx2 = max(a.mx2, b.mx2), res.mxc = a.mxc + b.mxc;
+    else if (a.mx1 > b.mx1) res.mx2 = max(a.mx2, b.mx1), res.mxc = a.mxc;
+    else res.mx2 = max(a.mx1, b.mx2), res.mxc = b.mxc;
+    res.mn1 = min(a.mn1, b.mn1);
+    if (a.mn1 == b.mn1) res.mn2 = min(a.mn2, b.mn2), res.mnc = a.mnc + b.mnc;
+    else if (a.mn1 < b.mn1) res.mn2 = min(a.mn2, b.mn1), res.mnc = a.mnc;
+    else res.mn2 = min(a.mn1, b.mn2), res.mnc = b.mnc;
+    res.sum = a.sum + b.sum; res.lazy = 0;
+    res.l = a.l; res.r = b.r;
+    return res;
   }
-  void init1(int l, int r, int idx, ll val) {
-    a[idx].l = l, a[idx].r = r;
-    if(l == r) {
-      a[idx].max1 = a[idx].min1 = val;
-      a[idx].max2 = -extr;
-      a[idx].min2 = extr;
-      a[idx].maxc = a[idx].minc = 1;
-      a[idx].lazy = 0;
-      a[idx].sum = val;
-      return;
-    }
-    int mid = (l+r) >> 1;
-    init1(l, mid, (idx<<1)+1, val);
-    init1(mid+1, r, (idx<<1)+2, val);
-    pushup(idx);
-  }
-  void u1(int l, int r, int constl, int constr, int idx, ll v) {
-    if (v >= a[idx].max1) return;
-    if (l<=constl && constr<=r && v>a[idx].max2) {
-      pushtag_max(idx, v); return;
-    }
-    pushdown(idx);
-    int mid = (constl+constr) >> 1;
-    if (mid < l || r < constl) u1(l, r, mid+1, constr, (idx<<1)+2, v);
-    else if (constr < l || r < mid+1) u1(l, r, constl, mid, (idx<<1)+1, v);
-    else {
-      u1(l, r, constl, mid, (idx<<1)+1, v);
-      u1(l, r, mid+1, constr, (idx<<1)+2, v);
-    }
-    pushup(idx);
-  }
-  void u2(int l, int r, int constl, int constr, int idx, ll v) {
-    if (v <= a[idx].min1) return;
-    if (l<=constl && constr<=r && v<a[idx].min2) {
-      pushtag_min(idx, v); return;
-    }
-    pushdown(idx);
-    int mid = (constl+constr) >> 1;
-    if (mid < l || r < constl) u2(l, r, mid+1, constr, (idx<<1)+2, v);
-    else if (constr < l || r < mid+1) u2(l, r, constl, mid, (idx<<1)+1, v);
-    else {
-      u2(l, r, constl, mid, (idx<<1)+1, v);
-      u2(l, r, mid+1, constr, (idx<<1)+2, v);
-    }
-    pushup(idx);
-  }
-  void u3(int l, int r, int constl, int constr, int idx, ll v) {
+  void upd(int l, int r, int constl, int constr, int idx, int v, Type type) {
+    if (type == CHMIN && st[idx].mx1 <= v) return;
+    if (type == CHMAX && st[idx].mn1 >= v) return;
     if (l <= constl && constr <= r) {
-      pushtag_add(idx, v);
-      return;
+      if (type == CHMIN && st[idx].mx2 < v) {
+        apply_chmin(st[idx], v); return;
+      }
+      if (type == CHMAX && st[idx].mn2 > v) {
+        apply_chmax(st[idx], v); return;
+      }
+      if (type == ADD) {
+        apply_add(st[idx], v); return;
+      }
     }
-    pushdown(idx);
-    int mid = (constl+constr) >> 1;
-    if (mid < l || r < constl) u3(l, r, mid+1, constr, (idx<<1)+2, v);
-    else if (constr < l || r < mid+1) u3(l, r, constl, mid, (idx<<1)+1, v);
+    push_down(idx);
+    int mid = (constl + constr) >> 1;
+    if (mid < l || r < constl) upd(l, r, mid + 1, constr, (idx << 1) + 2, v, type);
+    else if (constr < l || r < mid + 1) upd(l, r, constl, mid, (idx << 1) + 1, v, type);
     else {
-      u3(l, r, constl, mid, (idx<<1)+1, v);
-      u3(l, r, mid+1, constr, (idx<<1)+2, v);
+      upd(l, r, constl, mid, (idx << 1) + 1, v, type);
+      upd(l, r, mid + 1, constr, (idx << 1) + 2, v, type);
     }
-    pushup(idx);
+    st[idx] = push_up(st[(idx << 1) + 1], st[(idx << 1) + 2]);
   }
-  ll qu(int l, int r, int constl, int constr, int idx) {
-    if(l <= constl && constr <= r) {
-      return a[idx].sum;
-    }
-    pushdown(idx);
-    int mid = (constl+constr) >> 1;
-    if (mid < l || r < constl) return qu(l, r, mid+1, constr, (idx<<1)+2);
-    else if (constr < l || r < mid+1) return qu(l, r, constl, mid, (idx<<1)+1);
+  int qu(int l, int r, int constl, int constr, int idx) {
+    if (l <= constl && constr <= r) return st[idx].sum;
+    push_down(idx);
+    int mid = (constl + constr) >> 1;
+    if (mid < l || r < constl) return qu(l, r, mid + 1, constr, (idx << 1) + 2);
+    else if (constr < l || r < mid + 1) return qu(l, r, constl, mid, (idx << 1) + 1);
     else {
-      return qu(l, r, constl, mid, (idx<<1)+1) + qu(l, r, mid+1, constr, (idx<<1)+2);
+      return qu(l, r, constl, mid, (idx << 1) + 1) + qu(l, r, mid + 1, constr, (idx << 1) + 2);
     }
   }
   public:
-  SegtreeBeats(int k) : stok(k), a(vector<node>(4*k + 10)) {
-    init1(0, stok, 0, 0);
+  void init(int idx, int l, int r) {
+    st[idx] = {0, -INF, r - l + 1, 0, INF, r - l + 1, 0, 0, l, r};
+    if (l == r) return;
+    int mid = (l + r) >> 1;
+    init((idx << 1) + 1, l, mid);
+    init((idx << 1) + 2, mid + 1, r);
   }
-  void range_chmin(int l, int r, ll v) { u1(l, r, 0, stok, 0, v); }
-  void range_chmax(int l, int r, ll v) { u2(l, r, 0, stok, 0, v); }
-  void range_add(int l, int r, ll v) { u3(l, r, 0, stok, 0, v); }
-  ll query_sum(int l, int r) { return (ll) qu(l, r, 0, stok, 0); }
+  SegtreeBeats(int n) : n(n), st(4 * n + 20) { init(0, 0, n); }
+  void range_chmin(int l, int r, int v) { upd(l, r, 0, n, 0, v, CHMIN); }
+  void range_chmax(int l, int r, int v) { upd(l, r, 0, n, 0, v, CHMAX); }
+  void range_add(int l, int r, int v) { upd(l, r, 0, n, 0, v, ADD); }
+  int query_sum(int l, int r) { return qu(l, r, 0, n, 0); }
 };
