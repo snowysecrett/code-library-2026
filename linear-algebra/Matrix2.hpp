2444fc76ebba5ef8d1763f579af203f55e96726c
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-03-25 09:51:58 +0000

    Add Matrix struct with multiplication and power functions

---
+++ linear-algebra/Matrix2.hpp
@@ -0,0 +1,39 @@
+struct Matrix {
+  vector<vector<int>> a;
+  int n;
+  Matrix(int n) : n(n) {
+    a.resize(n);
+    for (int i=0; i<n; i++) a[i].resize(n);
+  }
+  Matrix operator*(Matrix other) {
+    Matrix product(n);
+    __int128 res[n][n];
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        res[i][j] = 0;
+      }
+    }
+    for (int i=0; i<n; i++) {
+      for (int k=0; k<n; k++) {
+        if (!a[i][k]) continue;
+        for (int j=0; j<n; j++) {
+          if (!other.a[k][j]) continue;
+          res[i][j] += a[i][k] * other.a[k][j];
+        }
+      }
+    }
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<n; j++) {
+        product.a[i][j] = res[i][j] % MOD;
+      }
+    }
+    
+    return product;
+  }
+};
+Matrix pow(Matrix b, int p) {
+  if (p == 1) return b;
+  Matrix r = pow(b, p >> 1);
+  if (p & 1) return r * r * b;
+  return r * r;
+}
