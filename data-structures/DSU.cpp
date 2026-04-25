73ee7a700fc7e3e7357e126c6b9205db03f77133
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-04-25 13:53:55 +0100

    Refactor DSU structure to use size instead of rank

--- data-structures/DSU.cpp
+++ data-structures/DSU.cpp
@@ -1,19 +1,21 @@
 struct DSU {
-  vector<int> dsu, rank;
+  vector<int> dsu, sz;
   int n;
   DSU() {}
-  DSU(int n) : n(n), dsu(vi(n)), rank(vi(n)) {
-    for (int i=0; i<n; i++) dsu[i] = i;
+  DSU(int n) : n(n), dsu(n), sz(n, 1) {
+    for (int i = 0; i < n; i++) dsu[i] = i;
   }
   int set_of(int u) {
-    while (dsu[u] != u) u = dsu[u];
-    return u;
+    if (dsu[u] == u) return u;
+    return dsu[u] = set_of(dsu[u]); // path compression
   }
-  void Union(int u, int v) {
-    u = set_of(u), v = set_of(v);
-    if (u == v) return;
-    if (rank[u] < rank[v]) swap(u, v);
+  bool Union(int u, int v) {
+    u = set_of(u);
+    v = set_of(v);
+    if (u == v) return false;
+    if (sz[u] < sz[v]) swap(u, v);
     dsu[v] = u;
-    if (rank[u] == rank[v]) rank[u]++;
+    sz[u] += sz[v];
+    return true;
   }
 };
