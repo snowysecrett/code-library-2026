6700aa3ae9653381f59969f26f2c14b2c0adb0ae
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-04-25 13:52:46 +0100

    Implement DSU structure

---
+++ data-structures/DSU.cpp
@@ -0,0 +1,19 @@
+struct DSURollback {
+  vector<int> dsu, rank;
+  int n;
+  DSURollback() {}
+  DSURollback(int n) : n(n), dsu(vi(n)), rank(vi(n)) {
+    for (int i=0; i<n; i++) dsu[i] = i;
+  }
+  int set_of(int u) {
+    while (dsu[u] != u) u = dsu[u];
+    return u;
+  }
+  void Union(int u, int v) {
+    u = set_of(u), v = set_of(v);
+    if (u == v) return;
+    if (rank[u] < rank[v]) swap(u, v);
+    dsu[v] = u;
+    if (rank[u] == rank[v]) rank[u]++;
+  }
+};
