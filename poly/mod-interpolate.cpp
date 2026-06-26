215ae7e44721a145975f71f0745da14f8dbe834c
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-06-26 18:24:04 +0100

    Implement Lagrange interpolation function

---
+++ poly/mod-interpolate.cpp
@@ -0,0 +1,14 @@
+vi interpolate(vi x, vi y, int n) {
+	vi res(n), temp(n);
+	rep(k,0,n-1) rep(i,k+1,n)
+		y[i] = (y[i] - y[k] + MOD) * modinv(x[i] - x[k] + MOD) % MOD; // modinv can be replaced with inv
+	int last = 0; temp[0] = 1;
+	rep(k,0,n) rep(i,0,n) {
+		res[i] += y[k] * temp[i] % MOD;
+        res[i] %= MOD;
+		swap(last, temp[i]);
+		temp[i] -= last * x[k] % MOD;
+        temp[i] = (temp[i] + MOD) % MOD;
+	}
+	return res;
+}
