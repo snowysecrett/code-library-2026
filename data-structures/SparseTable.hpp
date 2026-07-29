4865fb0e345ddd6c149b3c5a7d604fc4b60fdcce
Isaac Chan <94776938+snowysecrett@users.noreply.github.com>
2026-07-30 00:39:57 +0100

    Refactor SparseTable to use generic type T

--- data-structures/SparseTable.hpp
+++ data-structures/SparseTable.hpp
@@ -1,9 +1,11 @@
+template<typename T>
 struct SparseTable {
   int n;
-  vector<pii> st[20];
+  vector<pair<T, int32_t>> st[20];
   SparseTable() {}
-  SparseTable(vi v) {
+  SparseTable(vector<T> v) {
     n = sz(v);
+    for (int i=0; i<20; i++) st[i] = vector<pair<T, int32_t>>(n);
     for (int i=0; i<n; i++) st[0][i] = {v[i], i};
     for (int i=1; i<20; i++) {
       for (int j=0; j+(1<<i)-1<n; j++) {
@@ -11,11 +13,11 @@ struct SparseTable {
       }
     }
   }
-  int query_max(int l, int r) {
+  T query_max(int l, int r) {
     int k = 31-__builtin_clz(r-l+1);
     return max(st[k][l], st[k][r-(1<<k)+1]).fi;
   }
-  int query_argmax(int l, int r) {
+  int32_t query_argmax(int l, int r) {
     int k = 31-__builtin_clz(r-l+1);
     return max(st[k][l], st[k][r-(1<<k)+1]).se;
   }
